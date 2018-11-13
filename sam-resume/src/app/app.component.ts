d4b1753d80a92af3d1a01dc27ba6544a5db15a01
SamHuo213 <40151580+SamHuo213@users.noreply.github.com>
2018-11-12 19:33:18 -0800

    Mobile support of navbar (#3)
    * initial work for mobile navbar
    
    * fixed some linter errors
    
    * removed angular ico

--- sam-resume/src/app/app.component.ts
+++ sam-resume/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { WindowResizeTrackerClass } from './helpers/window-resize-tracker.class';
+import { SmallScreen } from './constants/small-screen.constant';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent extends WindowResizeTrackerClass {
+	smallScreen = SmallScreen;
+}
---
+++ sam-resume/src/app/constants/small-screen.constant.ts
@@ -0,0 +1,1 @@
+export const SmallScreen = 767;

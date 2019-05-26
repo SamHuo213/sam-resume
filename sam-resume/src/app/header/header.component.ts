ce7eba7d92870120e5183705739f5ab2ecf13f7e
Sam Huo <hhuo213@hotmail.com>
2019-05-26 11:12:40 -0700

    Download hardcopy of resume

--- sam-resume/src/app/header/header.component.ts
+++ sam-resume/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SmallScreen } from '../constants/small-screen.constant';
 import { WindowResizeTrackerClass } from '../helpers/window-resize-tracker.class';
+import { saveAs } from 'file-saver';
 
 @Component({
 	selector: 'header',
@@ -9,4 +10,11 @@ import { WindowResizeTrackerClass } from '../helpers/window-resize-tracker.class
 })
 export class HeaderComponent extends WindowResizeTrackerClass {
 	smallScreen = SmallScreen;
+
+	fileName = 'SamResume.pdf';
+	filePath = `assets/${this.fileName}`;
+
+	exportResume() {
+		saveAs(this.filePath, this.fileName);
+	}
 }
